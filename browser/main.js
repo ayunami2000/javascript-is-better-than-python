81c0eece61d9745a14ab401ca9f961088c6987b7
ayunami2000 <spwilliamsiam@gmail.com>
2019-10-25 09:47:45 -0400

    Update main.js

--- browser/main.js
+++ browser/main.js
@@ -2,3 +2,5 @@ console.log("console.log(...)");
 console.info("console.info(...)");
 console.warn("console.warn(...)");
 console.error("console.error(...)");
+//-----------------
+console.log("This is the script output!!");
